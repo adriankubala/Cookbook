a06e7f45ce93e92634e72248ec07d7c377f5969b
Adrian Kubała <adrian.kubala@gmail.com>
2018-11-26 17:44:12 +0100

    [develop] Translate dev data

--- cookbook/src/test/java/com/adriankubala/uni/cookbook/ingredient/IngredientFixtures.java
+++ cookbook/src/test/java/com/adriankubala/uni/cookbook/ingredient/IngredientFixtures.java
@@ -4,7 +4,7 @@ public final class IngredientFixtures {
 
 	public static final int EXISTING_INGREDIENTS_SIZE = 10;
 
-	public static final String EXISTING_INGREDIENT_NAME = "Jajka";
+	public static final String EXISTING_INGREDIENT_NAME = "Eggs";
 	public static final String NON_EXISTING_INGREDIENT_NAME = "1 butter";
 	public static final String INGREDIENT_NAME_WITH_WHITESPACES = " " + NON_EXISTING_INGREDIENT_NAME + " ";
 	public static final String INGREDIENT_NAME_WITH_NON_ALPHABETIC_CHAR = NON_EXISTING_INGREDIENT_NAME + "!";
